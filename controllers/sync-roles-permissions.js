3f52b5f7e90919beb8e5f3491a3401f6b9c1fb43
Alan <alan2207@live.com>
2021-04-24 22:04:23 +0200

    move logic into services

--- controllers/sync-roles-permissions.js
+++ controllers/sync-roles-permissions.js
@@ -1,18 +1,4 @@
 "use strict";
-const { sanitizeEntity } = require("strapi-utils");
-const yup = require("yup");
-
-const roleSchema = yup.object().shape({
-  name: yup.string().required(),
-  description: yup.string().required(),
-  type: yup.string().required(),
-  permissions: yup.object({
-    application: yup.object({}),
-    "content-manager": yup.object({}),
-    "content-type-builder": yup.object({}),
-    "users-permissions": yup.object({}),
-  }),
-});
 
 /**
  * sync-roles-permissions.js controller
@@ -38,66 +24,27 @@ module.exports = {
 
   getRoles: async (ctx) => {
     const { user } = ctx.state;
-    const service =
-      strapi.plugins["users-permissions"].services.userspermissions;
 
-    if (user.roles[0].id != 1) {
+    if (user.roles[0].name !== "Super Admin") {
       return ctx.unauthorized("You must be admin to access this resource!");
     }
 
-    const [roles, plugins] = await Promise.all([
-      service.getRoles(),
-      service.getPlugins(),
-    ]);
-
-    const rolesWithPermissions = await Promise.all(
-      roles.map(async (role) => await service.getRole(role.id, plugins))
-    );
-
-    return rolesWithPermissions.map((role) =>
-      sanitizeEntity(role, {
-        model: strapi.plugins["users-permissions"].models.role,
-      })
-    );
+    return strapi.plugins["sync-roles-permissions"].services[
+      "sync-roles-permissions"
+    ].getRoles();
   },
 
   updateRoles: async (ctx) => {
-    const { roles } = ctx.request.body;
     const { user } = ctx.state;
+    const { roles } = ctx.request.body;
 
-    const service =
-      strapi.plugins["users-permissions"].services.userspermissions;
-
-    if (user.roles[0].id != 1) {
+    if (user.roles[0].name !== "Super Admin") {
       return ctx.unauthorized("You must be admin to access this resource!");
     }
 
-    const roleValues = Object.values(roles);
-
-    const isValidJSON = await Promise.all(
-      roleValues.map(async (role) => {
-        return roleSchema.isValid(role);
-      })
-    ).then((values) => values.every(Boolean));
-
-    if (!isValidJSON) {
-      return ctx.throw(400, "Please provide a valid JSON");
-    }
-
-    await Promise.all(
-      roleValues.map(async (data) => {
-        const role = await strapi
-          .query("role", "users-permissions")
-          .findOne({ name: data.name });
-        const users = role ? role.users : [];
-        data.users = users;
-        if (!role) {
-          await service.createRole(data);
-        } else {
-          await service.updateRole(role.id, data);
-        }
-      })
-    );
+    await strapi.plugins["sync-roles-permissions"].services[
+      "sync-roles-permissions"
+    ].updateRoles(roles);
 
     return { success: true };
   },
--- services/sync-roles-permissions.js
+++ services/sync-roles-permissions.js
@@ -1,4 +1,18 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+const yup = require("yup");
+
+const roleSchema = yup.object().shape({
+  name: yup.string().required(),
+  description: yup.string().required(),
+  type: yup.string().required(),
+  permissions: yup.object({
+    application: yup.object({}),
+    "content-manager": yup.object({}),
+    "content-type-builder": yup.object({}),
+    "users-permissions": yup.object({}),
+  }),
+});
 
 /**
  * sync-roles-permissions.js service
@@ -7,5 +21,55 @@
  */
 
 module.exports = {
+  getRoles: async () => {
+    const service =
+      strapi.plugins["users-permissions"].services.userspermissions;
 
+    const [roles, plugins] = await Promise.all([
+      service.getRoles(),
+      service.getPlugins(),
+    ]);
+
+    const rolesWithPermissions = await Promise.all(
+      roles.map(async (role) => await service.getRole(role.id, plugins))
+    );
+
+    return rolesWithPermissions.map((role) =>
+      sanitizeEntity(role, {
+        model: strapi.plugins["users-permissions"].models.role,
+      })
+    );
+  },
+
+  updateRoles: async (roles) => {
+    const service =
+      strapi.plugins["users-permissions"].services.userspermissions;
+
+    const roleValues = Object.values(roles);
+
+    const isValidJSON = await Promise.all(
+      roleValues.map(async (role) => {
+        return roleSchema.isValid(role);
+      })
+    ).then((values) => values.every(Boolean));
+
+    if (!isValidJSON) {
+      return ctx.throw(400, "Please provide a valid JSON");
+    }
+
+    await Promise.all(
+      roleValues.map(async (data) => {
+        const role = await strapi
+          .query("role", "users-permissions")
+          .findOne({ name: data.name });
+        const users = role ? role.users : [];
+        data.users = users;
+        if (!role) {
+          await service.createRole(data);
+        } else {
+          await service.updateRole(role.id, data);
+        }
+      })
+    );
+  },
 };
